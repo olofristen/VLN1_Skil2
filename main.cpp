9b0f81ce8b416e581b7a32714ebb3599448a5d06
olofristen <olofristen@gmail.com>
2015-12-03 17:35:36 +0000

    Add main

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <QCoreApplication>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+#include "ui.h"
+
+
+using namespace std;
+
+int main()
+{
+    UI ui;
+
+    char check = 'y';
+
+    do{
+        ui.welcome();
+        ui.choices();
+
+        cout << endl;
+        cout << "Do you wish to continue? (Y/N) " << endl;
+        cin >> check;
+        system("CLS");
+
+    } while(check == 'y' || check == 'Y');
+
+
+}
